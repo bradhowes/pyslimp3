61de20679bb8d121f5b74667a5f315b46dba04e0
ahbeezee <ahbeezee@1371e4d0-6067-11de-8d08-39e76c6f0fc3>
2009-08-15 03:29:31 +0000

    Changed copyright information.

--- qtsim/VFDElement.h
+++ qtsim/VFDElement.h
@@ -2,8 +2,22 @@
 #define PYSLIMP3_VFDELEMENT_H
 
 //
-// (C) Copyright 2009 Massachusetts Institute of Technology. All rights
-// reserved.
+// (C) Copyright 2009 Brad Howes. All rights reserved.
+//
+// This file is part of Pyslimp3.
+//
+// Pyslimp3 is free software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 3, or (at your option) any later version.
+//
+// Pyslimp3 is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Pyslimp3; see the file COPYING. If not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
 //
 
 #include <QtDesigner/QDesignerExportWidget>
